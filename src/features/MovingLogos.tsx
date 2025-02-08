2304a6bf838df4ff0731b09b4b99092ae4ccc2fd
Jeff <dssdom@gmail.com>
2025-02-08 13:43:32 +0100

    fix: logos

--- src/features/MovingLogos.tsx
+++ src/features/MovingLogos.tsx
@@ -3,21 +3,21 @@
 import Marquee from "react-fast-marquee";
 import LogoBankOfAmerica from "@/shared/assets/logos/Logo - Bank of America.svg";
 import LogoBMO from "@/shared/assets/logos/Logo - BMO.svg";
-// import LogoCerenceAi from "@/shared/assets/logos/Logo - Cerence ai.svg";
+import LogoCerenceAi from "@/shared/assets/logos/Logo - Cerence ai.svg";
 import LogoCHASE from "@/shared/assets/logos/Logo - CHASE.svg";
-// import LogoColonialConsulting from "@/shared/assets/logos/Logo - Colonial consulting.svg";
-// import LogoCVC from "@/shared/assets/logos/Logo - CVC.svg";
+import LogoColonialConsulting from "@/shared/assets/logos/Logo - Colonial consulting.svg";
+import LogoCVC from "@/shared/assets/logos/Logo - CVC.svg";
 import LogoHarman from "@/shared/assets/logos/Logo - Harman.svg";
-// import LogoHarvestPartners from "@/shared/assets/logos/Logo - Harvest Partners.svg";
-// import LogoLeumi from "@/shared/assets/logos/Logo - Leumi.svg";
+import LogoHarvestPartners from "@/shared/assets/logos/Logo - Harvest Partners.svg";
+import LogoLeumi from "@/shared/assets/logos/Logo - Leumi.svg";
 import LogoMcKinsey from "@/shared/assets/logos/Logo - McKinsey & Company.svg";
 import LogoMSCI from "@/shared/assets/logos/Logo - MSCI.svg";
 import { cn } from "@/shared/lib/utils";
-// import LogoPSP from "@/shared/assets/logos/Logo - PSP.svg";
-// import LogoRosenberg from "@/shared/assets/logos/Logo - Rosenberg & Estis.svg";
-// import LogoSymphonyAi from "@/shared/assets/logos/Logo - SymphonyAI.svg";
-// import LogoTheUniversityOfChicago from "@/shared/assets/logos/Logo - The university of Chicago.svg";
-// import LogoZak from "@/shared/assets/logos/Logo - Zak.svg";
+import LogoPSP from "@/shared/assets/logos/Logo - PSP.svg";
+import LogoRosenberg from "@/shared/assets/logos/Logo - Rosenberg & Estis.svg";
+import LogoSymphonyAi from "@/shared/assets/logos/Logo - SymphonyAI.svg";
+import LogoTheUniversityOfChicago from "@/shared/assets/logos/Logo - The university of Chicago.svg";
+import LogoZak from "@/shared/assets/logos/Logo - Zak.svg";
 
 export const MovingLogos = ({ className }: { className?: string }) => {
   return (
@@ -37,18 +37,18 @@ const logos = [
   Marquee,
   LogoBankOfAmerica,
   LogoBMO,
-  // LogoCerenceAi,
+  LogoCerenceAi,
   LogoCHASE,
-  // LogoColonialConsulting,
-  // LogoCVC,
+  LogoColonialConsulting,
+  LogoCVC,
   LogoHarman,
-  // LogoHarvestPartners,
-  // LogoLeumi,
+  LogoHarvestPartners,
+  LogoLeumi,
   LogoMcKinsey,
   LogoMSCI,
-  // LogoPSP,
-  // LogoRosenberg,
-  // LogoSymphonyAi,
-  // LogoTheUniversityOfChicago,
-  // LogoZak,
+  LogoPSP,
+  LogoRosenberg,
+  LogoSymphonyAi,
+  LogoTheUniversityOfChicago,
+  LogoZak,
 ];
